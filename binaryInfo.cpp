c8310e2e9371334823224055aca786a5a71981c9
fabio <fabiodl@gmail.com>
2020-01-12 20:30:32 +0900

    added delay check

--- binaryInfo.cpp
+++ binaryInfo.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <map>
 #include <set>
+#include <vector>
 
 static const int CHANNELS=8;
 static size_t freq=32000000;
@@ -73,9 +74,182 @@ bool info(const char* name,size_t unstableT=0){
   
 }
 
+
+
+
+static inline bool getAsel(uint8_t v){
+  return v&1;
+}
+
+static inline bool getFdc(uint8_t v){
+  return v&(1<<1);
+}
+
+static inline bool getRom(uint8_t v){
+  return v&(1<<2);
+}
+
+static inline bool getRas2(uint8_t v){
+  return v&(1<<3);
+}
+
+static inline bool getCas2(uint8_t v){
+  return v&(1<<4);
+}
+
+static inline bool getCas0(uint8_t v){
+  return v&(1<<5);
+}
+
+static inline bool  getWr(uint8_t v){
+  return v&(1<<6);
+}
+
+
+static inline bool getTrdir(uint8_t v){
+  return v&1;
+}
+
+
+static inline bool getTrce(uint8_t v){
+  return v&(1<<1);
+}
+
+
+
+static inline bool getFfcp(uint8_t v){
+  return v&(1<<2);
+}
+
+static inline bool getFfoe(uint8_t v){
+  return v&(1<<3);
+}
+
+static inline bool getCdcas0(uint8_t v){
+  return v&(1<<4);
+}
+
+
+static inline bool getO6(uint8_t v){
+  return v&(1<<5);
+}
+
+static inline bool getO7(uint8_t v){
+  return v&(1<<6);
+}
+
+static inline bool getO8(uint8_t v){
+  return v&(1<<7);
+}
+
+
+std::vector<uint8_t> inp;
+std::vector<uint8_t> out;
+
+bool loadConv(const char* name,size_t unstableT=0){
+  ifstream file(name, ios::in|ios::binary|ios::ate);
+  if (file.is_open())
+  {
+    size_t size = file.tellg();
+    inp.resize(size/4-unstableT);
+    out.resize(size/4-unstableT);
+    file.seekg (0, ios::beg);
+
+    char zeros[4];
+
+    for (size_t i=0;i<unstableT;i++){
+      file.read(zeros,4);
+    }
+    
+    for (size_t i=0;i<size-unstableT;i++){
+      file.read(reinterpret_cast<char*>(inp.data()+i),1);
+      file.read(reinterpret_cast<char*>(out.data()+i),1);
+      file.read(zeros,2);
+    }            
+    return true;
+  }
   
+  return false;
+}
+
+
+
+struct Delay{
+  size_t mind,maxd;
+
+  Delay():
+    mind(0xFFF),maxd(0){}
+
+
+  void push(size_t d){
+    mind=min(d,mind);
+    maxd=max(d,maxd);
+  }
+
+};
+
+
+std::ostream& operator<<(std::ostream& o,const Delay& d){
+  return o<<d.mind<<"~"<<d.maxd;
+}
+
+
+struct EdgeDelay{
+  Delay up,down;
+  void push(size_t d,bool v){
+    if (v){
+      up.push(d);
+    }else{
+      down.push(d);
+    }
+  }
+  
+};
+
+std::ostream& operator<<(std::ostream& o,const EdgeDelay& d){
+  return o<<"up "<<d.up<<" down "<<d.down;
+}
+
+
+void checkDelay(){
+
+  EdgeDelay cas0,ras2,ffoe;
+  
+  for (size_t i=1;i<inp.size();i++){
+    
+    if  (getCas0(inp[i])!=getCas0(inp[i-1])){
+      size_t j;
+      for (j=0;getCdcas0(out[i+j])==getCas0(inp[i]);j++);      
+      //cout<<"j"<<j<<endl;
+      cas0.push(j,getCas0(inp[i]));
+    }
+
+    if  (getRas2(inp[i])!=getRas2(inp[i-1])){
+      size_t j;
+      for (j=0;getO6(out[i+j])!=getRas2(inp[i]);j++);            
+      ras2.push(j,getRas2(inp[i]));
+    }
+
+    if (getFfoe(out[i])!=getFfoe(out[i-1])){
+      size_t j;
+      for (j=0;getTrce(out[i+j])==getFfoe(out[i]);j++);            
+      ffoe.push(j,getFfoe(out[i]));
+    }
+
+    
+  }
+  cout<<"cas0 "<<cas0<<endl
+      <<"ras2 "<<ras2<< endl
+      <<"ffoe "<<ffoe<<endl;
+
+    ;
+  
+}
+
+
 int main(int argc,char** argv){
-  
-  info(argv[1],3*freq);
+  loadConv(argv[1],10);
+  cout<<"size"<<inp.size()<<endl;
+  checkDelay();
   
 }
